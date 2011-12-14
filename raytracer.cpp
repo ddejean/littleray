ce82b08c543ca670f20c4a8c30c6b0e823e5c515
Damien <djod4556@yahoo.fr>
2011-12-14 22:13:14 +0100

    Rendering: mesure rendering time.

--- raytracer.cpp
+++ raytracer.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sys/time.h>
 
 #include "Display.h"
 #include "Scene.h"
@@ -9,6 +10,10 @@ int main(int argc, char **argv)
         Display *display;
         SceneFactory *sceneFactory;
         Scene *scene;
+        /* Time evaluation */
+        struct timeval before, after;
+        double seconds, useconds;
+
 
         /* Create the scene and the screen */
         if (argc != 2) { 
@@ -22,7 +27,18 @@ int main(int argc, char **argv)
         }
 
         /* Render the picture */
+        gettimeofday(&before, NULL);
         scene->render(display);
+        gettimeofday(&after, NULL);
+        seconds = (double) (after.tv_sec  - before.tv_sec);
+        useconds = (double) (after.tv_usec - before.tv_usec);
+
+        std::cout << "Rendering time: "
+        		  << (seconds * 1000.0) + (useconds / 1000.0)
+        		  << " ms.\n"
+        		  << std::endl;
+
+        /* Display */
         display->refresh();
         display->waitForQuit();
 
